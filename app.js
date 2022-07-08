fb9b032cd27614e7376e97a2857da52233cc4a88
Baraa Halabi <baraaelhalabi@gmail.com>
2022-07-08 16:27:50 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-let tileDisplay = document.querySelector('.tile-container');
-let keyboard = document.querySelector('.key-container');
+const tileDisplay = document.querySelector('.tile-container');
+const keyboard = document.querySelector('.key-container');
+const messageDisplay = document.querySelector('.message-container');
 
 const wordle = 'SMILE';
 
@@ -57,15 +58,64 @@ for(let i = 0; i < guessRows.length; i ++) {
     tileDisplay.appendChild(row);
 }
 
-keys.forEach(key => {
+for(let i = 0; i < keys.length; i ++) {
     const keyButton = document.createElement('button');
-    keyButton.innerHTML = key;
-    keyButton.id = key;
-    keyButton.onclick = () => { handleClick(key) };
+    keyButton.innerHTML = keys[i];
+    keyButton.id = keys[i];
+    keyButton.onclick = () => { handleClick(keys[i]) };
 
     keyboard.appendChild(keyButton);
-});
+}
+
+// keys.forEach(key => {
+//     const keyButton = document.createElement('button');
+//     keyButton.innerHTML = key;
+//     keyButton.id = key;
+//     keyButton.onclick = () => { handleClick(key) };
+
+//     keyboard.appendChild(keyButton);
+// });
 
 let handleClick = (key) => {
-    console.log('you clicked ' + key);
+    if(key == 'ENTER') checkRow();
+    else if(key == '<') deleteLetter();
+    else addLetter(key);
+}
+
+let currentRow = 0, currentTile = 0;
+
+const addLetter = (letter) => {
+    if (currentTile >= 5 || currentRow >= 6) return;
+
+    const tile = document.getElementById('guess-row-' + currentRow + '-tile-' + currentTile);
+    tile.innerHTML = letter;
+    guessRows[currentRow][currentTile] = letter;
+    currentTile ++;
+}
+
+const deleteLetter = () => {
+    if(currentTile <= 0) return;
+
+    currentTile --;
+    const tile = document.getElementById('guess-row-' + currentRow + '-tile-' + currentTile);
+    tile.innerHTML = '';
+    guessRows[currentRow][currentTile] = '';
+}
+
+const checkRow = () => {
+    if(currentTile === 5) {
+        const guess = guessRows[currentRow].join('');
+        if(guess == wordle) {
+            showMessage('Magnificent!');
+        }
+    }
+}
+
+
+const showMessage = (message) => {
+    const messageElement = document.createElement('p');
+    messageElement.innerHTML = message;
+    messageDisplay.appendChild(messageElement);
+
+    setTimeout(() => {messageDisplay.removeChild(messageElement)}, 2000);
 }
